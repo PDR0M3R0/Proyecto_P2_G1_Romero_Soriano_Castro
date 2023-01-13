ae67085b559677afc8cc8739796ac6be063a486a
Paul David Romero <pdromero@espol.edu.ec>
2023-01-13 01:54:32 -0500

    -Edicion de la vista IngresoSistema en scene builder - Asignacion de nodos en la vista en el controlador con sus respectivos nombres y metodos

--- Proyecto_2P_SorianoAlexander_RomeroPaul/src/main/java/com/mycompany/proyecto_2p_sorianoalexander_romeropaul/IngresoSistemaController.java
+++ Proyecto_2P_SorianoAlexander_RomeroPaul/src/main/java/com/mycompany/proyecto_2p_sorianoalexander_romeropaul/IngresoSistemaController.java
@@ -2,7 +2,11 @@ package com.mycompany.proyecto_2p_sorianoalexander_romeropaul;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 
 /**
  * FXML Controller class
@@ -17,6 +21,25 @@ public class IngresoSistemaController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+        
+    }   
+    
+    @FXML
+    private ImageView seccionView1;
+    @FXML
+    private ImageView seccionView2;
+    @FXML
+    private Button btnIngresar;
+    @FXML
+    private TextField txtUsuario;
+    @FXML
+    private TextField txtContrasena;
+    
+    
+    @FXML
+    public void ingresar(){
+     
+        
+    }
     
 }
