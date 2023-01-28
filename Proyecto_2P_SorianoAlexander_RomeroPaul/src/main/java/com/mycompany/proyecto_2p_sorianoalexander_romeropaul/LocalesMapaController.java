266c895d7aaaf2c3e9cc25dccee868aa19486676
Alexander Soriano <axsorian@espol.edu.ec>
2023-01-27 22:51:27 -0500

    - Parte del mapa 90% completada, falta nomas ajustes de css

--- Proyecto_2P_SorianoAlexander_RomeroPaul/src/main/java/com/mycompany/proyecto_2p_sorianoalexander_romeropaul/LocalesMapaController.java
+++ Proyecto_2P_SorianoAlexander_RomeroPaul/src/main/java/com/mycompany/proyecto_2p_sorianoalexander_romeropaul/LocalesMapaController.java
@@ -9,7 +9,9 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -29,6 +31,7 @@ import javafx.scene.layout.StackPane;
 public class LocalesMapaController implements Initializable {
 
     private ArrayList<Local> local;
+    private ImageView imgv;
 
     @FXML
     private Pane rootPane;
@@ -40,35 +43,84 @@ public class LocalesMapaController implements Initializable {
     }
 
     void agregarImgview() {
+
         rootPane.getChildren().clear();
-        ImageView imgv = null;
-        for (Local loc : local) {
-            try ( FileInputStream input = new FileInputStream(Principal.pathImages + "icono.png")) {
-                Image img = new Image(input, 40, 40, false, false);
-                imgv = new ImageView(img);
+        imgv = null;
 
-                imgv.setX(loc.getCoordenadaX());
-                imgv.setY(loc.getCoordenadaY());
-                
-            } catch (IOException ex) {
+        Thread t = new Thread(new Runnable() {
+            public void run() {
 
-            }
-            
-            String name=loc.getNombre();
-            String direccion=loc.getDireccion();
-            String horario=loc.getHorario();
-            
-          
-            rootPane.getChildren().add(imgv);
-            imgv.setOnMouseClicked(new EventHandler<MouseEvent>(){
-                public void handle(MouseEvent e){
-                    Alert al=new Alert(AlertType.INFORMATION);
-                    al.showAndWait();
-                    
+                for (Local loc : local) {
+                    Random r = new Random();
+                    int n = r.nextInt(11);
+                    try {
+                        Thread.sleep(n * 1000);
+                    } catch (InterruptedException e) {
+                        System.out.println(e.getMessage());
+                    }
+
+                    Platform.runLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            try ( FileInputStream input = new FileInputStream(Principal.pathImages + "icono.png")) {
+                                Image img = new Image(input, 40, 40, false, false);
+                                imgv = new ImageView(img);
+
+                                imgv.setX(loc.getCoordenadaX());
+                                imgv.setY(loc.getCoordenadaY());
+
+                            } catch (IOException ex) {
+
+                            }
+
+                            String name = loc.getNombre();
+                            String direccion = loc.getDireccion();
+                            String horario = loc.getHorario();
+
+                            rootPane.getChildren().add(imgv);
+                            imgv.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                                public void handle(MouseEvent e) {
+                                    Alert al = new Alert(AlertType.INFORMATION);
+
+                                    Thread t2 = new Thread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            for (int i = 5; i >= 0; i--) {
+                                                int contador = i;
+                                                String finish = "La ventana se cerrara en " + i + " segundos";
+
+                                                try {
+                                                    Thread.sleep(1000);
+                                                } catch (InterruptedException ex) {
+
+                                                }
+                                                Platform.runLater(new Runnable() {
+
+                                                    @Override
+                                                    public void run() {
+                                                        al.setContentText(finish);
+                                                        if (contador == 0) {
+                                                            al.close();
+                                                        }
+                                                    }
+                                                });
+
+                                            }
+                                        }
+                                    });
+                                    t2.setDaemon(true);
+                                    t2.start();
+                                    al.showAndWait();
+                                }
+                            }
+                            );
+
+                        }
+
+                    });
                 }
-            });
-
-        }
+            }
+        });
+        t.start();
     }
-
 }
